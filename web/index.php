52d157eb695e0e88de501db923cd354a288758b3
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-10-09 11:10:38 +0200

    Removed code that wasn't working

--- web/index.php
+++ web/index.php
@@ -2,12 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
-    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    header("HTTP/1.1 301 Moved Permanently");
-    header("Location: $redirect");
-}
-
 if (! extension_loaded('openssl')) {
     die('You must enable the openssl extension.');
 }
