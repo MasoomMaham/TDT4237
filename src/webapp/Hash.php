3ac57ee4f870a0b60c4f224298d339cc1e093dea
killcam <camiberggren@gmail.com>
2015-10-19 12:26:55 +0200

    hash and salt for password

--- src/webapp/Hash.php
+++ src/webapp/Hash.php
@@ -7,8 +7,7 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 class Hash
 {
 
-    static $salt = "1234";
-
+    //static $salt = "1234";
 
     public function __construct()
     {
@@ -16,13 +15,21 @@ class Hash
 
     public static function make($plaintext)
     {
-        return hash('sha1', $plaintext . Hash::$salt);
-
+        //return hash('sha1', $plaintext . Hash::$salt);
+        $salt = "";
+        for ($i=0; $i < 10; $i++) { 
+            $str = str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345‌​6789");
+            $salt .= $str[0];
+        }
+        $hashsalt = crypt($plaintext, '$6$' . $salt) . $salt;
+        return $hashsalt;
     }
 
     public function check($plaintext, $hash)
     {
-        return $this->make($plaintext) === $hash;
+        //return $this->make($plaintext) === $hash;
+        $salt = substr($hash, -10);
+        $newhash = crypt($plaintext, '$6$' . $salt) . $salt;
+        return $newhash === $hash;
     }
-
 }
