a2c984aee265490a48402574aa7f2a8054cc885f
Masoom Maham <tdt4258@tdt4258-VirtualBox.(none)>
2015-10-21 11:48:43 +0200

    can no longer go to the login page when you are logged in

--- src/webapp/controllers/LoginController.php
+++ src/webapp/controllers/LoginController.php
@@ -26,6 +26,10 @@ class LoginController extends Controller
 
     public function login()
     {
+        if (!$this->auth->guest()) {
+            $this->app->flash('info', "You are already logged in");
+            $this->app->redirect('/');
+        }
         $request = $this->app->request;
         $user    = strtolower($request->post('user'));
         $pass    = $request->post('pass');
