f4e29c5cad7fa32c97eb8bbea1a8c2fb9d09555b
Masoom Maham <masoommaham@gmail.com>
2015-10-22 15:16:35 +0200

    added isdoctor for temp

--- src/webapp/controllers/Controller.php
+++ src/webapp/controllers/Controller.php
@@ -27,6 +27,7 @@ class Controller
             $variables['isLoggedIn'] = true;
             $variables['isAdmin'] = $this->auth->isAdmin();
             $variables['loggedInUsername'] = $_SESSION['user'];
+            $variables['isDoctor'] = $_SESSION['user'];
         }
 
         print $this->app->render($template, $variables);
