929b88e27bc729ef3a7ae51b2ae49d7d40e309b6
asjaddev@gmail.com <asjaddev@gmail.com>
2015-10-22 23:59:24 +0200

    Last commit

--- src/webapp/validation/sqlValidation.php
+++ src/webapp/validation/sqlValidation.php
@@ -14,24 +14,20 @@ class sqlValidation
 	public static function whiteBlackListSQL($input)
 	{
 		$input = strtolower($input);
-		$whiteList = "abcdefghijklmnopqrstuvwxyzæøå1234567890!@#£¤$%&/(){}[]?±*^~§|¦:.,-_";
-		$blackList = ["and","or","concat","=",";","from users", "from posts", "from comments"];
+		$whiteList = "abcdefghijklmnopqrstuvwxyzæøå1234567890!@#£¤$%&/(){}[]?±*^~§|¦:.,-_ ";
+		//$blackList = [" and "," or "," concat "," = "," ; "," from users ", " from posts ", " from comments "];
 
-		for($i = 0; $i < sizeof($blackList); $i++)
-		{
-			if((strpos($input, $blackList[$i])) === true)
-			{
+		/*for($i = 0; $i < sizeof($blackList); $i++) {
+			if((strpos($input, $blackList[$i])) === true){
 				return false;
 			}
-		}
+		}*/
 
-		for($i = 0; $i < sizeof($whiteList); $i++)
-		{
-			if(!(strpos($input, $whiteList[$i])) === false)
-			{
+		/*for($i = 0; $i < sizeof($whiteList); $i++) {
+			if(!(strpos($input, $whiteList[$i])) === false){
 				return false;
 			}
-		}
+		}*/
 
 
 		return true;
